9ad34ad559a5cf1bceba43fc4be3c800cb95d10c
Adrian Batista <adrianfnb@gmail.com>
2023-10-15 14:30:57 -0300

    fix color changing

--- color_change.js
+++ color_change.js
@@ -1,15 +1,16 @@
-
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  const letters = "0123456789ABCDEF"; //Letters won't change, so it is a constant
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-  function changeHeaderColor()
-  {
-    colorInput=getRandomColor()
-    Headers.style.color=colorInput;
-  }
-  setInterval('changeHeaderColor()',500)
+  return color;
+}
+
+function changeHeaderColor() {
+  const colorInput = getRandomColor();
+  const element = document.getElementById("magicText"); //Get dom's element with ID
+  element.style.color = colorInput;
+}
+
+setInterval(changeHeaderColor, 500); //Here you pass the function, not the string with the name
